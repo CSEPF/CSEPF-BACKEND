0bf16ea47087a666122489eaf5821d0a20acc5a8
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-08-18 11:39:46 +0000

    chore: Update Nginx proxy_pass URLs for CSEPF subdomains

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -12,7 +12,7 @@ server {
     server_name api.csepf.org;
 
     location / {
-        proxy_pass http://35.175.146.251:8000;
+        proxy_pass http://35.175.146.251:8000/api;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -38,20 +38,7 @@ server {
     server_name su.csepf.org;
 
     location / {
-        proxy_pass http://35.175.146.251:8000;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-}
-
-server {
-    listen 80;
-    server_name csepf.org;
-
-    location / {
-        proxy_pass http://35.175.146.251:8000;
+        proxy_pass http://35.175.146.251:8000/su;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
